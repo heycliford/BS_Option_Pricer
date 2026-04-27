b72a0918bb4e0993b6e92ef60ce58313b077839f
Clifton Winiecki <cltwin03@yahoo.com>
2026-04-27 13:51:30 -0400

    added main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+# Black Scholes Option Pricing
+
+
+# IMPORTS
+import math
+
+
+def main() -> None:
+    y = 81
+
+    result = math.sqrt(y)
+
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
+
